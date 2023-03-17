8ff2d806a3596a339e1c63ea8bada3238e8c28d2
StipRabin <117095282+StipRabyn@users.noreply.github.com>
2023-03-18 00:15:33 +0300

    Update and rename bot.py to vk_bot.py

--- vk_bot.py
+++ vk_bot.py
@@ -2,7 +2,7 @@ from vkbottle.bot import Bot
 from handlers.common import common_labeler
 from handlers.admin_commands import admin_commands
 from handlers.admin_text import admin_text
-from api import (
+from vk_api import (
     api,
     labeler,
     callback)
