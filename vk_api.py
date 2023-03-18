093c4bb2e41d7b50b3f0556bf5b922343f69968d
StipRabin <117095282+StipRabyn@users.noreply.github.com>
2023-03-18 17:47:02 +0300

    Update vk_api.py

--- vk_api.py
+++ vk_api.py
@@ -2,7 +2,6 @@ from io import BytesIO
 from vkbottle.bot import BotLabeler
 from vkbottle.callback import BotCallback
 from vkbottle import (
-    BuiltinStateDispenser,
     PhotoMessageUploader,
     API)
 from config import (
@@ -15,7 +14,6 @@ from config import (
 api = API(TOKEN)
 labeler = BotLabeler()
 uploader = PhotoMessageUploader(api)
-state_dispenser = BuiltinStateDispenser()
 image_handler = BytesIO()
 
 callback = BotCallback(url=URL,
