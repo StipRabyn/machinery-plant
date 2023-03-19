9f2bf2ed50f14bb30476e84262a27e93d210192c
StipRabin <117095282+StipRabyn@users.noreply.github.com>
2023-03-19 15:27:06 +0300

    Update machines.py

--- machines.py
+++ machines.py
@@ -9,9 +9,9 @@ from config import (
 
 async def machine_units():
     # функция отправки сгенерированного юнита
-    async def send_unit(type_unit, unit):
-        await api.messages.send(peer_id=2000000002,
-                                message=f"Произведена единица {type_unit}: {unit}",
+    async def send_unit(emoji, type_unit, unit):
+        await api.messages.send(peer_id=2000000003,
+                                message=f"{emoji} Произведена единица {type_unit}: {unit}",
                                 random_id=0)
 
     # подключение к базе данных
@@ -39,20 +39,19 @@ async def machine_units():
 
             if random.randint(1, 4) == 2:
                 index = 0
-                await send_unit("военной техники", MACHINES['Военная техника'][machine][index])
+                await send_unit('✈', "военной техники", MACHINES['Военная техника'][machine][index])
             else:
                 if random.randint(1, 2) == 2:
                     index = 1
-                    await send_unit("военной техники", MACHINES['Военная техника'][machine][index])
+                    await send_unit('✈', "военной техники", MACHINES['Военная техника'][machine][index])
                 else:
                     index = 2
-                    await send_unit("военной техники", MACHINES['Военная техника'][machine][index])
+                    await send_unit('✈', "военной техники", MACHINES['Военная техника'][machine][index])
 
             await db.hincrby(machine, MACHINES['Военная техника'][machine][index], 1)
 
         # генерация гражданской техники
         else:
             machine = random.choice(MACHINES['Гражданская техника'])
-            await send_unit("гражданской техники", machine)
+            await send_unit('⛴', "гражданской техники", machine)
             await db.hincrby("Гражданская техника", machine, 1)
-    
