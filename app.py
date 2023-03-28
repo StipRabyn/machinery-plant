e11c4d1d7676f5a050ec2b4e04ed26964039be31
StipRabin <117095282+StipRabyn@users.noreply.github.com>
2023-03-28 21:54:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,8 @@ async def startup_function():
                                          int(time.strftime("%M", time.localtime())))
 
                 if time_now >= time_unit:
-                    if choice(tuple(range(1, 3))) == 1:
+                    random_number = choice(tuple(range(1, 4)))
+                    if random_number == 1 or random_number == 2:
                         await clock()
                         await machine_units(2000000002)
                     else:
